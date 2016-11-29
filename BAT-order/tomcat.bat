4452f648f0ff6739bc92c884f645d3f34a2fef06
kay <2826362725@qq.com>
2016-11-29 16:53:49 +0800

    Create tomcat.bat
    批处理一键启动多个 tomcat

---
+++ BAT-order/tomcat.bat
@@ -0,0 +1,13 @@
+rem 关闭提示命令输出
+@echo off
+rem 设置tomcat1的CATALINA_HOME
+set CATALINA_HOME="E:\apache-tomcat-6.0.10"
+call %CATALINA_HOME%\bin\startup.bat
+echo tomcat1 start
+rem 设置tomcat2的CATALINA_HOME
+set CATALINA_HOME="E:\apache-tomcat-6.0.10-8081"
+call %CATALINA_HOME%\bin\startup.bat
+echo tomcat2 start
+rem pause 窗口按任意键结束
+rem cmd 提示版本号
+pause
